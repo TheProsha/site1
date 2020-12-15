75fbdec48085ce50543855cc8c69a5024952ed15
TheProsha <76067918+TheProsha@users.noreply.github.com>
2020-12-15 22:10:15 +0300

    Add files via upload

---
+++ load.js
@@ -0,0 +1,9 @@
+function one(){
+    $( "#load_cont" ).load( "save_load.html #var_1" );
+}
+function two(){
+    $( "#load_cont" ).load( "save_load.html #var_2" );
+}
+function three(){
+    $( "#load_cont" ).load( "save_load.html #var_3" );
+}
