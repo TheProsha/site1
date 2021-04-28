c955953800961ecf5fcbe58ef361379c52d2aca8
TheProsha <76067918+TheProsha@users.noreply.github.com>
2021-04-28 18:22:29 +0300

    Delete load.js

--- load.js
+++
@@ -1,9 +0,0 @@
-function one(){
-    $( "#load_cont" ).load( "save_load.html #var_1" );
-}
-function two(){
-    $( "#load_cont" ).load( "save_load.html #var_2" );
-}
-function three(){
-    $( "#load_cont" ).load( "save_load.html #var_3" );
-}
